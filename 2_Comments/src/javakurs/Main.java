a51aae3e6a450bb271be61be8a7a637b83fb1024
alxmck <mail@alxmck.de>
2019-12-17 20:14:12 +0100

    created second project with comments

---
+++ 2_Comments/src/javakurs/Main.java
@@ -0,0 +1,32 @@
+/*
+ *  Written by Alexander Mack (github.com/alxmck). Have fun using the code where ever you want.
+ */
+package javakurs;
+
+/**
+ *
+ * @author Alexander Mack (github.com/alxmck)
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        // Kommentare helfen den Quellcode besser lesbar zu machen
+        // und das Programm auch spaeter wieder zu verstehen.
+        
+        // Einzeilige Kommentare werden mit dem doppelten // eingeleitet
+        
+        /*
+        Moechte man nicht fuer jede Zeile die // setzen, kann man auch 
+        hiermit 
+        arbeiten
+        */
+        
+        System.out.println("Das ist kein Kommentar, sondern eine Textausgabe.");
+        
+        // Wichtig ist nur zu verstehen, dass Kommentare nicht ausgefuehrt oder sonst eine Auswirkung haben.
+        // Sie helfen einfach nur, das Programmierte besser zu verstehen.
+        
+    }
+    
+}
